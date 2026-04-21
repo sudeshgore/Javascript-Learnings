a8b4eb364319973bc9f012e4ea7825d8bbf4494b
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-21 12:09:51 +0530

    Data-types learnings

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hi I'm Pop-up for browsers")  Only works on browser to show pop-up.
+
+
+// Primitive data-types
+let name = "Sudesh" // String data-type
+let age = 23 // Number data-type (Range of "number" data-type is 2^53)
+// for greater than 2^53 "bigint" is used.(Trading, Banking sector)
+let isLoggedIn = true // Boolean data-type
+
+let city; // Here value is not defined. It is "undefined data-type"
+let state = null // null => standalone value(Empty, not zero)
+
+// "symbol" data-type => unique 
+/*
+    const sym1 = Symbol('description'); 
+    const sym2 = Symbol('description');
+    console.log(sym1 === sym2); // false
+*/
+
+// "Object" =>
+    
+
+// "typeof" is used to check the data type.
+console.log(typeof name); // return string
+console.log(typeof null); // return object
+console.log(typeof undefined); //return undefined
+
+
+
+
