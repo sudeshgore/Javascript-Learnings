553aa329833fb272e9ac8edcdd4a342adf79d0d6
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-21 11:32:42 +0530

    Variables-Learnings

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,14 +1,20 @@
 // Different types to declare variables
+
 // 1st way- Cannot be re-initailized.i.e, Fix value.
 const accountId = 144553
 // accountId = 2 -> Not allowed.
+
+
 // 2nd way
 let accountEmail = "sudeshgoredev@gmail.com"
-// 3rd way
+
+
+// 3rd way- Prefer not to use because of global scope
 var accountPassword = "12345"
 // 4th way
-accountCity = "Pune"
+accountCity = "Pune";
+let accountState;  // Give "Undefined"
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
