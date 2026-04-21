af3e26fc5ee8b6b2f9af594d7e6cf6ece1c9f78d
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-21 11:21:30 +0530

    Variables learnings

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+// Different types to declare variables
+// 1st way- Cannot be re-initailized.i.e, Fix value.
+const accountId = 144553
+// accountId = 2 -> Not allowed.
+// 2nd way
+let accountEmail = "sudeshgoredev@gmail.com"
+// 3rd way
+var accountPassword = "12345"
+// 4th way
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
