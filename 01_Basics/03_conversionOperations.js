4c8123fe3d86cb41b4a0e4b768e5726fe95f3778
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-21 13:05:03 +0530

    Type-conversion

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,4 +1,44 @@
-let score = 33
+// let score = true
  
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score); // number (lowercase)
+// console.log(typeof(score)); // number (lowercase)
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+    After Conversion into Number:-
+        "33" => dataType-number, value- 33
+        "33abc" => dataType-number, value- NaN
+        null => dataType- number, value- 0
+        undefined => dataType-number, value- NaN
+        true => dataTypes-number, value-1
+*/
+
+// let name = true;
+// console.log(typeof name);
+// console.log(name);
+
+// let valueInBool = Boolean(name)
+// console.log(typeof valueInBool); 
+// console.log(valueInBool);
+
+/*
+    After Conversion into Boolean:-
+        "" => dataType- boolean, value-false
+        "Sudesh"=> dataType- boolean, value- true
+        null => dataType- boolean, value- false
+        undefined => dataType- undefined, value- false
+        true => dataType- boolean, value- true
+*/ 
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // dataType - string
+
+
+
+
