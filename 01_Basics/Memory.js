01e0fb48134e0dbdbbc58ac09f5036f8255da8da
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-24 15:03:43 +0530

    Memory usage

--- 01_Basics/Memory.js
+++ 01_Basics/Memory.js
@@ -8,3 +8,24 @@ console.log(myYoutubename); // sudeshgoredotcom
 console.log(anothername); // return "Altname"
 
 
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sudeshgore@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
+
+/* 
+    Get stored in heap and userOne and userTwo points to following
+     {
+        email: "user@google.com",
+        upi: "user@ybl"
+    }
+
+    if we make changes userOne or userTwo then it will reflect in both.
+*/
