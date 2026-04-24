af271855433c6d6e2c8d3e616adf6922752c3367
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-24 13:06:04 +0530

    String operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -33,12 +33,52 @@
         true => dataType- boolean, value- true
 */ 
 
-let someNumber = 45
+// let someNumber = 45
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber); // dataType - string
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber); // dataType - string
+
+// *****************Operations*****************
+
+let value = 3
+let negValue = -value // assigning negative value
+// console.log(negValue)
 
 
+// Basic arithmetic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3); // => same as 2^3 = 8
+// console.log(2**2);
+// -----------
+// console.log(2/3);
+// console.log(Math.ceil(2/3)); // Ceiling value
+// console.log(Math.floor(2/3)); // floor value
+// console.log((2/3).toFixed(2)); // Give value upto two decimal places 
+
+// ---------------
+// console.log(2%3);
+
+// -----------String operations----------->
+
+
+// Concatenation of string- 1st way
+// let  str1 = "Hello"
+// let str2 = ", Sudesh"
+
+// let str3 = str1 + str2  // concatenation of strings
+// console.log(str3);
+
+// Concatenation of string- 2nd way
+
+// let str1 = "Sudesh"
+// let  str2 = "Hello"
+// console.log(`Hello, ${str1}!`);
+
+// Concatenation of string- 3rd way
+// let str1 = "Hello, "
+//  str1 += "Sudesh!"
+// console.log(str1);
 
 
