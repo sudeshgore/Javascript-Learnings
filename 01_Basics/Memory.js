c2393c63b153747c263f2810da5cdb2a2f1c46ef
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-24 15:00:04 +0530

    Memory

---
+++ 01_Basics/Memory.js
@@ -0,0 +1,10 @@
+let myYoutubename = "sudeshgoredotcom"
+
+let anothername = myYoutubename // copy of "myYoutubename" get stored in "anothername"
+
+anothername = "Altname" // updating value of "anothername"
+
+console.log(myYoutubename); // sudeshgoredotcom
+console.log(anothername); // return "Altname"
+
+
