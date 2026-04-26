7b238179edec4d9919a44d2060fe09ffef2fdbcd
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-26 15:29:14 +0530

    Number&Maths

---
+++ 01_Basics/06-Nums&Maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log((balance.toString().length));
+// console.log((balance.toFixed(1)));
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString()); 
+// console.log(hundreds.toLocaleString('en-IN')); // Indian standards
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.sqrt(36));
+console.log(Math.min(4, 3, 6, 7, 8));
+console.log(Math.max(4, 3, 6, 7, 8));
+console.log(Math.random()); // value is always between 0 and 1
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
+
+
+
+
