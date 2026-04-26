d69028da19e57d141a87b32eeb40c6da9435d163
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-26 15:00:20 +0530

    String

--- 01_Basics/05_String.js
+++ 01_Basics/05_String.js
@@ -6,16 +6,36 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 // Another way to declare string
-const gameName = new String('sudesh')
+const gameName = new String('sudesh-sg-gore')
 // console.log(typeof gameName);
 
 // Methods of String
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('d'));
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('d'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(0, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   sudesh   "
+// console.log(newStringOne.trim());
+
+// const newStringTwo = "Sudesh \n gore" 
+// console.log(newStringTwo);
+
+// const newStringThree = "https://sudesh.com/sudesh%20gore"
+// console.log(newStringThree.replace('%20', '-'));
+
+// console.log(newStringThree.includes('sudesh'));
+
+console.log(gameName.split('-'));
 
 
