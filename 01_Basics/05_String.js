6a881a30b154a9d865887d18c1dab0bf248b2393
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-26 14:45:14 +0530

    String

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,21 @@
+const name = "abcd"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// Another way to declare string
+const gameName = new String('sudesh')
+// console.log(typeof gameName);
+
+// Methods of String
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('d'));
+
+
