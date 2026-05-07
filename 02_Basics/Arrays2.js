27b2ba845310debcdd2e59d51ef1c9f444835eb3
Sudesh Gore <sudeshgore1112@gmail.com>
2026-05-07 14:07:04 +0530

    Arrays part 2

---
+++ 02_Basics/Arrays2.js
@@ -0,0 +1,9 @@
+const marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Black Adam"]
+
+console.log(marvel_heros);
+console.log(dc_heros);
+
+marvel_heros.push(dc_heros)
+
+
